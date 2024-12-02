9bb253bf8dfb4f792830730f82e4ff258a813bce
Razan Srour <68164671+Srourrzan@users.noreply.github.com>
2024-12-02 11:24:41 +0000

    create ft_list.h

---
+++ level04/ft_list_foreach/ft_list.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void    ft_list_foreach(t_list *begin_list, void (*f)(void *));
+
+typedef struct      s_list
+{
+    struct  s_list  *next;
+    void            *data;
+}                   t_list;
