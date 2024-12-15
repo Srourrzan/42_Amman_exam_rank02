ede2b7d9debde872a6f2b897ce2c80c3d5eaf824
Razan Srour <razanmsrour@gmail.com>
2024-12-15 17:49:07 +0300

    create ft_union function

--- level02/union.c
+++ level02/union.c
@@ -1,3 +1,48 @@
 #include <unistd.h>
 #include <stdio.h>
 
+char    ft_strchr(char *src, int c)
+{
+    while (*src != '\0' && *src != (unsigned char)c)
+        src++;
+    if (*src == (unsigned char)c)
+        return (c);
+    return ('\0');
+}
+
+void    ft_union(char *src1, char *src2)
+{
+    char    str[27];
+    int     iter1 = 0;
+    int     iter2 = 0;
+
+    while(src1[iter1])
+    {
+        if (src1[iter1] != ft_strchr(str, src1[iter1]))
+        {
+            str[iter2] = src1[iter1];
+            iter2++;
+        }
+        iter1++;
+    }
+    iter1 = 0;
+    while(src2[iter1])
+    {
+        if (src2[iter1] != ft_strchr(str, src2[iter1]))
+        {
+            str[iter2] = src2[iter1];
+            iter2++;
+        }
+        iter1++;
+    }
+    str[iter2]='\0';
+    printf("%s\n", str);
+}
+
+int     main(int argc, char **argv)
+{
+    if (argc == 3)
+        ft_union(argv[1], argv[2]);
+    write(1, "\n", 1);
+    return (0);
+}
