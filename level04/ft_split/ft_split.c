96ef31fa38f07f63107a61a81edf9d918238dc55
Razan Srour <68164671+Srourrzan@users.noreply.github.com>
2024-12-17 10:42:39 +0000

    create ft_num_words function

--- level04/ft_split/ft_split.c
+++ level04/ft_split/ft_split.c
@@ -17,20 +17,50 @@ int     ft_strlen(char *str)
     return len;
 }
 
+int     ft_num_words(char *str)
+{
+    int     counter;
+    int     word_bool;
+    int     iter;
+
+    iter = 0;
+    counter = 0;
+    word_bool = 0;
+    while (str[iter])
+    {
+        if ((str[iter] != '\n' || str[iter] != ' ') && (word_bool == 0))
+        {
+            word_bool = 1;
+            counter++;
+        }
+        else if (str[iter] == '\n' || str[iter] == ' ')
+            word_bool = 0;
+        iter++;
+    }
+    return (counter);
+}
+
 char    **ft_split(char *str)
 {
-    char **words;
-    int len = 0;
+    //char **words;
+    //int len = 0;
+    int     num_words;
 
     if (!str)
         return (NULL);
-    len = ft_strlen(str);
-    words = malloc(sizeof(char *)*len);
-    
+    //len = ft_strlen(str);
+    //words = malloc(sizeof(char *)*len);
+    num_words = ft_num_words(str);
+    printf("number of words: %d\n", num_words);
+    return NULL;
 }
 
-int     main(char **argv)
+int     main(int argc, char **argv)
 {
-    char  **words = ft_split(argv[1]);
+    if (argc == 2)
+    {
+        char  **words = ft_split(argv[1]);
+        printf("%s\n", words[0]);
+    }
     return (0);
 }
