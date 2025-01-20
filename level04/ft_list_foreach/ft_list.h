5b5487e0afe67672aa73bc2169d1bbf30d41e7ea
Razan Srour <68164671+Srourrzan@users.noreply.github.com>
2025-01-20 08:47:55 +0000

    remove git conflict lines

--- level04/ft_list_foreach/ft_list.h
+++ level04/ft_list_foreach/ft_list.h
@@ -1,20 +1,6 @@
-<<<<<<< HEAD
-#include <unistd.h>
-#include <stdio.h>
-=======
 #ifndef FT_LIST_H
 # define FT_LIST_H
->>>>>>> e65f1ec52dca75abfcb1996634f68912743e0f5e
 
-<<<<<<< HEAD
-void    ft_list_foreach(t_list *begin_list, void (*f)(void *));
-
-typedef struct      s_list
-{
-    struct  s_list  *next;
-    void            *data;
-}                   t_list;
-=======
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -32,4 +18,3 @@ char    ft_toupper(char c);
 void    ft_lst_toupper(void *data);
 
 #endif
->>>>>>> e65f1ec52dca75abfcb1996634f68912743e0f5e
